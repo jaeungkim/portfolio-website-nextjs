687653b6a423c8115c7f9058bef76d108824a230
jaeungkim <jaeung95@student.ubc.ca>
2023-02-24 16:33:53 -0800

    230224 04:33pm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import Link from "next/link";
-
+import Box from "../components/box";
+import { Canvas } from "@react-three/fiber";
 export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>Jaeung Kim</title>
       </Head>
-      <div>
-        <p className="text-3xl font-bold underline">Hello</p>
-      </div>
+      <Canvas>
+        <color attach="background" args={["#fff"]} />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
       <Link href={`/blog`}>Blog</Link>
     </Layout>
   );
