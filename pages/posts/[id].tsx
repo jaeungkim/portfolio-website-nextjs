c528c1613d4207cff7e0dd80ed8cfb417d6686a8
jaeungkim <jaeung95@student.ubc.ca>
2023-03-06 15:01:36 -0800

    230306 03:01pm

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Date from "../../components/shared/date";
 import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
+import { useRouter } from "next/router";
 
 export default function Post({
   postData,
@@ -14,16 +15,35 @@ export default function Post({
     contentHtml: string;
   };
 }) {
+  const router = useRouter();
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
+      <button
+        onClick={() => router.back()}
+        type="button"
+        aria-label="Go back to articles"
+        className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
+      >
+        <svg
+          viewBox="0 0 16 16"
+          fill="none"
+          aria-hidden="true"
+          className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400"
+        >
+          <path
+            d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </button>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
+        <div className={utilStyles.lightText}></div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
