104ccacdea5bfdad50e41eecbf057f1b8fa422c5
jaeungkim <jaeung95@student.ubc.ca>
2023-03-06 16:58:03 -0800

    open graph test

--- components/shared/layout.tsx
+++ components/shared/layout.tsx
@@ -20,9 +20,7 @@ export default function Layout({
         <meta name="description" content="Jaeung Kim Blog" />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://vercel-og-nextjs-og-image-generation-chi.vercel.app/`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
