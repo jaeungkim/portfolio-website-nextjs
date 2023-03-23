887389c6cf3d0d8feefec6404d649e89dc340f4a
jaeungkim <jaeung95@student.ubc.ca>
2023-03-23 16:44:42 -0700

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,51 @@
 import "../styles/global.css";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { ThemeProvider } from "next-themes";
-import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-function App({ Component, pageProps, router }: AppProps) {
+function App({ Component, pageProps }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setIsAnimating(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setIsAnimating(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router]);
+
   return (
-    <AnimatePresence mode="wait">
+    <ThemeProvider enableSystem={true} attribute="class">
       <motion.div
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-            transition: {
-              duration: 0.75,
-            },
-          },
-        }}
-        exit={{
-          opacity: 0,
-          transition: {
-            duration: 0.75,
-          },
-        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.75 }}
       >
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <Component {...pageProps} />
       </motion.div>
-    </AnimatePresence>
+      {isAnimating && (
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.75 }}
+        />
+      )}
+    </ThemeProvider>
   );
 }
 
