f500311f87428af5d44529fc5fd42df917720f03
jaeungkim <jaeung95@student.ubc.ca>
2023-05-17 16:01:51 -0700

    230517 04:01pm

--- components/projects/ProjectSingle.tsx
+++ components/projects/ProjectSingle.tsx
@@ -4,19 +4,17 @@ import Link from "next/link";
 const ProjectSingle = ({ id, img, title, date, videoSrc }: any) => (
   <Link href={`/project/${id}`} passHref>
     <motion.div
-      className="overflow-hidden rounded-md p-1 shadow-zinc-800/5 ring-1 ring-zinc-900/5 shadow-lg hover:shadow-xl cursor-pointer mb-4 sm:mb-0 bg-white/90 dark:bg-zinc-800/90"
+      className="will-change-auto overflow-hidden rounded-md p-1 shadow-zinc-800/5 ring-1 ring-zinc-900/5 shadow-lg hover:shadow-xl cursor-pointer mb-4 sm:mb-0 bg-white/90 dark:bg-zinc-800/90"
       aria-label="Single Project"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.7,
+        delay: 0.15,
+      }}
     >
-      <motion.div
-        // initial={{ opacity: 0 }}
-        // animate={{ opacity: 1 }}
-        // transition={{
-        //   ease: "easeInOut",
-        //   duration: 0.7,
-        //   delay: 0.15,
-        // }}
-        className="w-full h-full relative rounded-md overflow-hidden"
-      >
+      <motion.div className="will-change-auto w-full h-full relative rounded-md overflow-hidden">
         <video
           muted
           autoPlay
@@ -27,7 +25,7 @@ const ProjectSingle = ({ id, img, title, date, videoSrc }: any) => (
         >
           <source src={videoSrc} type="video/mp4"></source>
         </video>
-        <div className="absolute bottom-0 left-0 w-full h-full flex justify-between items-end gap-1 p-3 z-20 transition-opacity duration-300 will-change-auto bg-gradient-to-t from-black to-transparent">
+        <div className="absolute bottom-0 left-0 w-full h-full flex justify-between items-end gap-1 p-3 z-20 will-change-auto transition-opacity duration-300 bg-gradient-to-t from-black to-transparent">
           <h3 className="text-xs text-neutral-200 flex gap-1 justify-start items-center">
             {title}
           </h3>
