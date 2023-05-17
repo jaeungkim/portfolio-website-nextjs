0d8de2727fc5affd8f668141d60cb5e383a34ed0
jaeungkim <jaeung95@student.ubc.ca>
2023-05-17 13:45:12 -0700

    230517 01:45pm

--- components/projects/ProjectSingle.tsx
+++ components/projects/ProjectSingle.tsx
@@ -5,9 +5,7 @@ import Link from "next/link";
 const ProjectSingle = ({ id, img, title, category }) => (
   <Link href={`/project/${id}`} passHref>
     <motion.div
-      // whileHover={{ scale: 1.05 }}
-      // whileTap={{ scale: 0.95 }}
-      className="overflow-hidden rounded-md p-1 shadow-zinc-800/5 ring-1 ring-zinc-900/5 shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-white/90 dark:bg-zinc-800/90"
+      className="overflow-hidden rounded-md p-1 shadow-zinc-800/5 ring-1 ring-zinc-900/5 shadow-lg hover:shadow-xl cursor-pointer mb-4 sm:mb-0 bg-white/90 dark:bg-zinc-800/90"
       aria-label="Single Project"
     >
       <motion.div
@@ -26,32 +24,6 @@ const ProjectSingle = ({ id, img, title, category }) => (
           className="w-full h-full object-cover object-center transform transition-transform duration-500 ease-in-out"
         />
       </motion.div>
-      {/* <div className="p-4">
-        <motion.p
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            ease: "easeInOut",
-            duration: 0.7,
-            delay: 0.15,
-          }}
-          className="text-md md:text-lg mb-2 text-zinc-800 dark:text-zinc-100 font-bold"
-        >
-          {title}
-        </motion.p>
-        <motion.span
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            ease: "easeInOut",
-            duration: 0.7,
-            delay: 0.3,
-          }}
-          className="text-md text-zinc-600 dark:text-zinc-400 font-medium uppercase tracking-wider"
-        >
-          {category}
-        </motion.span>
-      </div> */}
     </motion.div>
   </Link>
 );
--- components/projects/ProjectsGrid.tsx
+++ components/projects/ProjectsGrid.tsx
@@ -20,10 +20,29 @@ function ProjectsGrid() {
     <section className="mt-16 sm:mt-20">
       {/* <ProjectsFilter setSelectProject={setSelectProject} /> */}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+      {/* MOBILE */}
+      <div className="grid grid-cols-1 sm:hidden gap-2">
         {filteredProjects.map((project, index) => (
           <ProjectSingle key={index} {...project} />
         ))}
+      </div>
+
+      {/* TABLET OR BIGGER */}
+      <div className="hidden sm:flex gap-2">
+        <div className="flex flex-col gap-4">
+          {filteredProjects
+            .slice(0, Math.ceil(filteredProjects.length / 2))
+            .map((project, index) => (
+              <ProjectSingle key={index} {...project} />
+            ))}
+        </div>
+        <div className="flex flex-col gap-4">
+          {filteredProjects
+            .slice(Math.ceil(filteredProjects.length / 2))
+            .map((project, index) => (
+              <ProjectSingle key={index} {...project} />
+            ))}
+        </div>
       </div>
     </section>
   );
