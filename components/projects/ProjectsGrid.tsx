8f6602159421f5265c4f977a5940665e23f8c5e3
jaeungkim <jaeung95@student.ubc.ca>
2023-05-25 16:09:18 -0700

    update

--- components/projects/ProjectsGrid.tsx
+++ components/projects/ProjectsGrid.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import ProjectSingle from "./ProjectSingle";
 import { projectsData } from "../../data/projectsData";
-import ProjectsFilter from "./ProjectsFilter";
-import { easeInOut, motion } from "framer-motion";
+
 function ProjectsGrid() {
   const [selectProject, setSelectProject] = useState("");
 
@@ -23,19 +25,47 @@ function ProjectsGrid() {
 
       {/* MOBILE */}
       <div className="grid grid-cols-1 sm:hidden gap-2">
-        {filteredProjects.map((project, index) => (
-          <ProjectSingle key={index} {...project} />
-        ))}
+        {filteredProjects.map((project, index) => {
+          const [ref, inView] = useInView({
+            triggerOnce: true,
+          });
+
+          const animationProps = {
+            initial: { opacity: 0, y: 20 },
+            animate: inView ? { opacity: 1, y: 0 } : {},
+            transition: { duration: 0.6, delay: index * 0.01 },
+          };
+
+          return (
+            <motion.div key={index} ref={ref} {...animationProps}>
+              <ProjectSingle {...project} />
+            </motion.div>
+          );
+        })}
       </div>
 
       {/* TABLET OR BIGGER */}
-      <motion.div className="hidden sm:flex gap-2">
+      <div className="hidden sm:flex gap-2">
         <div className="flex flex-col gap-4">
           {filteredProjects
             .slice(0, Math.ceil(filteredProjects.length / 2))
-            .map((project, index) => (
-              <ProjectSingle key={index} {...project} />
-            ))}
+            .map((project, index) => {
+              const [ref, inView] = useInView({
+                triggerOnce: true,
+              });
+
+              const animationProps = {
+                initial: { opacity: 0, y: 20 },
+                animate: inView ? { opacity: 1, y: 0 } : {},
+                transition: { duration: 0.6, delay: index * 0.01 },
+              };
+
+              return (
+                <motion.div key={index} ref={ref} {...animationProps}>
+                  <ProjectSingle {...project} />
+                </motion.div>
+              );
+            })}
         </div>
         <div className="flex flex-col gap-4">
           {filteredProjects
@@ -44,7 +74,7 @@ function ProjectsGrid() {
               <ProjectSingle key={index} {...project} />
             ))}
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 }
