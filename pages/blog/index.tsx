4e56f3589bf14d9a0df68da97f6a8501921bf9ec
jaeungkim <jaeung95@student.ubc.ca>
2023-05-28 18:31:25 -0700

    ok

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -93,7 +93,7 @@ export default function Blog({ allPostsData }: BlogProps) {
                         <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
                         <Link href={`/blog/${id}`}>
                           <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                          <p>{title}</p>
+                          <span className="relative z-10">{title}</span>
                         </Link>
                       </h2>
                       <Date dateString={date} mobile={true} />
