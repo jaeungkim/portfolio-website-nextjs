beb186e3b24a742d989f2d7cf11fba1be85a2ba6
jaeungkim <jaeung95@student.ubc.ca>
2023-06-06 11:19:18 -0700

    230606 11:19AM fixed inView function

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -7,6 +7,7 @@ import Layout from "../../components/shared/layout";
 import { getSortedPostsData } from "../../lib/posts";
 import Link from "next/link";
 import { useInView } from "react-intersection-observer";
+
 interface Post {
   id: string;
   title: string;
@@ -14,6 +15,62 @@ interface Post {
   summary: string;
   tags: string[];
 }
+
+interface ArticleProps {
+  post: Post;
+  index: number;
+}
+
+const Article: React.FC<ArticleProps> = ({ post, index }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <motion.article
+      ref={ref}
+      className="md:grid md:grid-cols-4 md:items-baseline"
+      initial={{ opacity: 0, y: 20 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.6, delay: index * 0.01 }}
+      key={post.id}
+    >
+      <div className="md:col-span-3 group relative flex flex-col items-start">
+        <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+          <Link href={`/blog/${post.id}`}>
+            <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+            <span className="relative z-10">{post.title}</span>
+          </Link>
+        </h2>
+        <Date dateString={post.date} mobile={true} />
+        <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+          {post.summary}
+        </p>
+        <div
+          aria-hidden="true"
+          className="relative z-10 mt-4 flex items-center text-sm font-medium text-cyan-500"
+        >
+          Read article
+          <svg
+            viewBox="0 0 16 16"
+            fill="none"
+            aria-hidden="true"
+            className="ml-1 h-4 w-4 stroke-current"
+          >
+            <path
+              d="M6.75 5.75 9.25 8l-2.5 2.25"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <Date dateString={post.date} mobile={false} />
+    </motion.article>
+  );
+};
 
 interface BlogProps {
   allPostsData: Post[];
@@ -72,58 +129,9 @@ export default function Blog({ allPostsData }: BlogProps) {
                 </button>
               ))}
             </div>
-
-            {allPostsData
-              .filter(postMatchesSelectedTags)
-              .map(({ id, date, summary, title, tags }, index) => {
-                const [ref, inView] = useInView({
-                  triggerOnce: true,
-                });
-                return (
-                  <motion.article
-                    ref={ref}
-                    className="md:grid md:grid-cols-4 md:items-baseline"
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={inView ? { opacity: 1, y: 0 } : {}}
-                    transition={{ duration: 0.6, delay: index * 0.01 }}
-                    key={id}
-                  >
-                    <div className="md:col-span-3 group relative flex flex-col items-start">
-                      <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                        <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
-                        <Link href={`/blog/${id}`}>
-                          <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                          <span className="relative z-10">{title}</span>
-                        </Link>
-                      </h2>
-                      <Date dateString={date} mobile={true} />
-                      <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                        {summary}
-                      </p>
-                      <div
-                        aria-hidden="true"
-                        className="relative z-10 mt-4 flex items-center text-sm font-medium text-cyan-500"
-                      >
-                        Read article
-                        <svg
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          aria-hidden="true"
-                          className="ml-1 h-4 w-4 stroke-current"
-                        >
-                          <path
-                            d="M6.75 5.75 9.25 8l-2.5 2.25"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          ></path>
-                        </svg>
-                      </div>
-                    </div>
-                    <Date dateString={date} mobile={false} />
-                  </motion.article>
-                );
-              })}
+            {allPostsData.filter(postMatchesSelectedTags).map((post, index) => (
+              <Article post={post} index={index} key={post.id} />
+            ))}
           </div>
         </div>
       </motion.div>
