e075301e7b1bdafe257e3d9e375c3e2894e13cdd
jaeungkim <jaewoongkim95@gmail.com>
2023-11-14 23:09:34 -0800

    230114 11:09pm updated introduce part - kr

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,7 +5,7 @@ import { HiOutlineMail } from "react-icons/hi";
 
 export default function About() {
   return (
-    <div className="relative text-3xl mt-9 md:px-4 sm:px-8 lg:px-12 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl mx-auto noto-font text-[#37352F]">
+    <div className="relative text-3xl mt-9 px-4 sm:px-8 lg:px-12 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl mx-auto noto-font text-[#37352F]">
       {/* HEADER  */}
       <div className="md:grid md:grid-cols-3 gap-4">
         <div className="flex justify-center md:justify-normal">
@@ -85,31 +85,26 @@ export default function About() {
         </div>
         <div className="col-span-2">
           <p className="text-base mb-4 font-normal">
-            열정적이고 협업을 중시하는 소프트웨어 엔지니어로서 2년간의 경험을
-            가지고 있습니다. 빠르고 다이나믹한 환경의 요구를 충족시킨
-            소프트웨어를 설계, 구축 및 유지 관리하는 데에 전문성을 갖추고
-            있습니다. 풀스택 웹 개발을 전문으로 하며, 신흥 기술을 활용하여
-            혁신적인 솔루션을 창출하는 데에 열정을 가지고 있습니다. 창의성,
-            협업, 그리고 지속적인 학습을 우선시하는 기술 지향적인 회사에서의
-            기회를 찾고 있습니다.
+            스타트업 환경에서 웹 서비스의 설계, 개발, 배포 및 운영을 통해 2년
+            간의 경험을 쌓은 프론트엔드 개발자로서, 웹 서비스에 생동감을
+            불어넣는 모션 구현을 통해 사용자 경험을 풍부하게 만드는 것을
+            좋아합니다. WebGL, GSAP, Animation 등을 활용해 효율적인 렌더링과
+            동시에 매력적인 인터랙션을 제공하는 데 주력합니다.
           </p>
           <p className="text-base mb-4">
-            열정적이고 협업을 중시하는 소프트웨어 엔지니어로서 2년간의 경험을
-            가지고 있습니다. 빠르고 다이나믹한 환경의 요구를 충족시킨
-            소프트웨어를 설계, 구축 및 유지 관리하는 데에 전문성을 갖추고
-            있습니다. 풀스택 웹 개발을 전문으로 하며, 신흥 기술을 활용하여
-            혁신적인 솔루션을 창출하는 데에 열정을 가지고 있습니다. 창의성,
-            협업, 그리고 지속적인 학습을 우선시하는 기술 지향적인 회사에서의
-            기회를 찾고 있습니다.
+            웹 기술의 빠른 변화에 발맞추어, 지속적인 학습을 통해 최신 트렌드,
+            보안 문제, 그리고 시스템 효율성에 관한 지식을 꾸준히 확장해왔습니다.
+            이러한 노력은 프로젝트에 혁신적인 아이디어를 적용하고 비즈니스의
+            성장을 도모하는 데 큰 도움이 되었습니다. 또한, 자동화와 프로세스
+            최적화에 집중함으로써 작업 효율성을 상당히 높일 수 있었습니다.
           </p>
           <p className="text-base">
-            열정적이고 협업을 중시하는 소프트웨어 엔지니어로서 2년간의 경험을
-            가지고 있습니다. 빠르고 다이나믹한 환경의 요구를 충족시킨
-            소프트웨어를 설계, 구축 및 유지 관리하는 데에 전문성을 갖추고
-            있습니다. 풀스택 웹 개발을 전문으로 하며, 신흥 기술을 활용하여
-            혁신적인 솔루션을 창출하는 데에 열정을 가지고 있습니다. 창의성,
-            협업, 그리고 지속적인 학습을 우선시하는 기술 지향적인 회사에서의
-            기회를 찾고 있습니다.
+            성공적인 프로젝트는 원활한 커뮤니케이션에서 비롯된다는 것을 이해하고
+            있습니다. 팀원들과의 페어 프로그래밍을 통해 지식과 아이디어를
+            공유하는 것을 즐기며, 모든 이들과 긍정적이고 열린 마음으로
+            소통합니다. 이러한 경험들은 저를 더 나은 개발자로 성장시키는
+            원동력이 되었고, 계속해서 새로운 기술을 배우고 전문성을 키우는 데
+            집중하고 있습니다.
           </p>
         </div>
       </div>
@@ -197,35 +192,24 @@ export default function About() {
           </p>
           <ul className="py-4 text-base font-normal pl-8 list-disc flex flex-col gap-2">
             <li>
-              MERN 스택과 Next.js를 사용하여 Shopify Marketplaces가 원활하게
-              통합된 정교한 전자상거래 플랫폼 개발
+              Developed enterprise-grade application, utilizing MEAN stack and
+              latest 3D technologies including WebGL, Three.js, and GSAP
             </li>
             <li>
-              Supabase 및 제3자 로그인 키트를 통해 플랫폼 보안 및 사용자 접근성
-              향상, 계정 보안 사고 40% 감소
+              Successfully implemented Redux for centralized state management,
+              ensuring efficient data flow, and reducing data retrieval times
             </li>
             <li>
-              AWS와 Fly.io로 전체 애플리케이션 생명주기 관리, 인프라 비용 30%
-              절감 및 시스템 신뢰성 향상
+              Architected scalable and maintainable server-side applications
+              using Node.js and other frameworks, supported by REST APIs
             </li>
             <li>
-              신속한 온보딩 과정을 촉진하여 소프트 런칭 당일 1개 이상의 상인이
-              참여, 초기 수익 창출에 기여
+              Designed a robust database schema and executed MongoDB queries
+              with precision, ensuring data consistency and integrity
             </li>
             <li>
-              Shopify Payments를 사용한 결제 게이트웨이 통합, 카트 포기 감소 및
-              성공적인 거래 증가
-            </li>
-            <li>
-              분석 및 보고 도구 구현, 데이터 주도 문화 및 의사결정 효율성 20%
-              개선
-            </li>
-            <li>
-              확장 가능하고 고성능 인프라 엔지니어링, 플랫폼 최적화로 트래픽 및
-              수요 증가 효율적 처리
-            </li>
-            <li>
-              UI/UX 개선 구현으로 사용자 참여 25% 증가 및 사용자 유지율 15% 상승
+              Automated deployment procedures with CI/CD, reducing build times
+              and enabling seamless deployment of new features and updates
             </li>
           </ul>
         </div>
@@ -253,35 +237,25 @@ export default function About() {
           </p>
           <ul className="py-4 text-base font-normal pl-8 list-disc flex flex-col gap-2">
             <li>
-              MERN 스택과 Next.js를 사용하여 Shopify Marketplaces가 원활하게
-              통합된 정교한 전자상거래 플랫폼 개발
+              Identified and addressed software defects in React single page
+              apps, ensuring high-quality code and seamless user experience
             </li>
             <li>
-              Supabase 및 제3자 로그인 키트를 통해 플랫폼 보안 및 사용자 접근성
-              향상, 계정 보안 사고 40% 감소
+              Assisted with the migration of legacy REST API to GraphQL with
+              Apollo, improving backend connection and app performance
             </li>
             <li>
-              AWS와 Fly.io로 전체 애플리케이션 생명주기 관리, 인프라 비용 30%
-              절감 및 시스템 신뢰성 향상
+              Refactored and optimized the frontend codebase to increase website
+              performance by 70% while adhering to SEO principles
             </li>
             <li>
-              신속한 온보딩 과정을 촉진하여 소프트 런칭 당일 1개 이상의 상인이
-              참여, 초기 수익 창출에 기여
+              Successfully integrated custom global components and frontend
+              libraries to support the development of future company products
             </li>
             <li>
-              Shopify Payments를 사용한 결제 게이트웨이 통합, 카트 포기 감소 및
-              성공적인 거래 증가
-            </li>
-            <li>
-              분석 및 보고 도구 구현, 데이터 주도 문화 및 의사결정 효율성 20%
-              개선
-            </li>
-            <li>
-              확장 가능하고 고성능 인프라 엔지니어링, 플랫폼 최적화로 트래픽 및
-              수요 증가 효율적 처리
-            </li>
-            <li>
-              UI/UX 개선 구현으로 사용자 참여 25% 증가 및 사용자 유지율 15% 상승
+              Integrated multiple third-party systems, including Mobilum,
+              enabling customers to use credit cards to purchase
+              cryptocurrencies
             </li>
           </ul>
         </div>
