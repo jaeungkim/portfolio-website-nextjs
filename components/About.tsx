bc4dfe825d378f31b611c657bec6d5f4c93a7de2
jaeungkim <jaewoongkim95@gmail.com>
2023-12-26 17:19:20 +0900

    231226 05:19pm refactored a little

--- components/About.tsx
+++ components/About.tsx
@@ -56,8 +56,6 @@ export default function About() {
         </Canvas>
       </div>
       <article className="md:basis-4/6 lg:basis-3/6">
-        {/* <h1 className="mb-4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-      </h1> */}
         <h2 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           <MemoizedCrypticTextDynamic
             text={`Hello I'm Jae`}
@@ -65,30 +63,13 @@ export default function About() {
             classNames={`${styles.fadein}`}
           />
         </h2>
-        {/* <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
-          I'm Jae 🧑🏻‍💻
-        </p> */}
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
-          I am a highly motivated and collaborative software engineer with 2
+          I am a highly motivated and collaborative software engineer with 3
           years of experience in designing, building, and maintaining software
           for startup companies. I primarily focus on full-stack web development
           but I also have experiences working as a designer and product owner
           depending on the needs of the company.
         </p>
-        {/* <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
-          웹 기술의 빠른 변화에 발맞추어, 지속적인 학습을 통해 최신 트렌드, 보안
-          문제, 그리고 시스템 효율성에 관한 지식을 꾸준히 확장해 왔습니다.
-          이러한 노력은 프로젝트에 혁신적인 아이디어를 적용하고 비즈니스의
-          성장을 도모하는 데 큰 도움이 되었습니다. 또한, 자동화와 프로세스
-          최적화에 집중함으로써 작업 효율성을 상당히 높일 수 있었습니다.
-        </p>
-        <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
-          성공적인 프로젝트는 원활한 커뮤니케이션에서 비롯된다는 것을 이해하고
-          있습니다. 팀원들과의 페어 프로그래밍을 통해 지식과 아이디어를 공유하는
-          것을 즐기며, 모든 이들과 긍정적이고 열린 마음으로 소통합니다. 이러한
-          경험들은 저를 더 나은 개발자로 성장시키는 원동력이 되었고, 계속해서
-          새로운 기술을 배우고 전문성을 키우는 데 집중하고 있습니다.
-        </p> */}
 
         <div className="mt-6 flex gap-6">
           <SocialIcon
--- components/projects/ProjectsGrid.tsx
+++ components/projects/ProjectsGrid.tsx
@@ -1,47 +1,44 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-
 import ProjectSingle from "./ProjectSingle";
 import { projectsData } from "@/data/projectsData";
 
 function ProjectsGrid() {
   const [selectProject, setSelectProject] = useState("");
 
-  const filteredProjects = projectsData.filter((project) => {
-    const category =
-      project.category.charAt(0).toUpperCase() + project.category.slice(1);
+  const capitalizeFirstLetter = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
 
-    if (selectProject && !category.includes(selectProject)) {
-      return false;
-    }
+  const filteredProjects = useMemo(
+    () =>
+      projectsData.filter((project) => {
+        const category = capitalizeFirstLetter(project.category);
+        return !selectProject || category.includes(selectProject);
+      }),
+    [selectProject]
+  );
 
-    return true;
-  });
+  const renderProject = (project, index) => {
+    const [ref, inView] = useInView({ triggerOnce: true });
+    const animationProps = {
+      initial: { opacity: 0, y: 20 },
+      animate: inView ? { opacity: 1, y: 0 } : {},
+      transition: { duration: 0.6, delay: index * 0.01 },
+    };
+
+    return (
+      <motion.div key={project.id} ref={ref} {...animationProps}>
+        <ProjectSingle {...project} />
+      </motion.div>
+    );
+  };
 
   return (
     <section className="mt-14 sm:mt-16">
-      {/* <ProjectsFilter setSelectProject={setSelectProject} /> */}
-
       {/* MOBILE */}
       <div className="grid grid-cols-1 sm:hidden gap-2">
-        {filteredProjects.map((project, index) => {
-          const [ref, inView] = useInView({
-            triggerOnce: true,
-          });
-
-          const animationProps = {
-            initial: { opacity: 0, y: 20 },
-            animate: inView ? { opacity: 1, y: 0 } : {},
-            transition: { duration: 0.6, delay: index * 0.01 },
-          };
-
-          return (
-            <motion.div key={index} ref={ref} {...animationProps}>
-              <ProjectSingle {...project} />
-            </motion.div>
-          );
-        })}
+        {filteredProjects.map(renderProject)}
       </div>
 
       {/* TABLET OR BIGGER */}
@@ -49,30 +46,12 @@ function ProjectsGrid() {
         <div className="flex flex-1 flex-col gap-4">
           {filteredProjects
             .slice(0, Math.ceil(filteredProjects.length / 2))
-            .map((project, index) => {
-              const [ref, inView] = useInView({
-                triggerOnce: true,
-              });
-
-              const animationProps = {
-                initial: { opacity: 0, y: 20 },
-                animate: inView ? { opacity: 1, y: 0 } : {},
-                transition: { duration: 0.6, delay: index * 0.01 },
-              };
-
-              return (
-                <motion.div key={index} ref={ref} {...animationProps}>
-                  <ProjectSingle {...project} />
-                </motion.div>
-              );
-            })}
+            .map(renderProject)}
         </div>
         <div className="flex flex-1 flex-col gap-4">
           {filteredProjects
             .slice(Math.ceil(filteredProjects.length / 2))
-            .map((project, index) => (
-              <ProjectSingle key={index} {...project} />
-            ))}
+            .map(renderProject)}
         </div>
       </div>
     </section>
