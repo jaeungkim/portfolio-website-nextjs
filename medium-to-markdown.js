02b48ad031137877e12284883e998167525b66b9
jaeungkim <jaewoongkim95@gmail.com>
2024-01-16 20:53:54 +0900

    240116 08:53pm updated travel blog..

--- medium-to-markdown.js
+++
@@ -1,6 +0,0 @@
-const mediumToMarkdown = require('medium-to-markdown');
-
-mediumToMarkdown.convertFromUrl('https://medium.com/@jaeungkim/remove-outermost-parentheses-695c3133b35')
-.then(function (markdown) {
-  console.log(markdown); //=> Markdown content of medium post
-});
