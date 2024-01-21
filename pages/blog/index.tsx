49cd0dac9c7c0ddeece81b3c43ec4fdf4fa01e42
jaeungkim <jaewoongkim95@gmail.com>
2024-01-21 15:13:16 +0900

    240121 03:13pm react-storage blog update

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,18 +1,17 @@
-import { GetStaticProps } from "next";
 import Head from "next/head";
 import { useState, useMemo, useEffect } from "react";
 import { motion } from "framer-motion";
 import Layout from "@/components/shared/layout";
-import { getSortedPostsData } from "@/lib/posts";
 import Article from "@/components/blog/Article";
-
+import { getSortedPostsData } from "@/lib/posts";
+import { GetStaticProps } from "next";
 interface Post {
   id: string;
   title: string;
   date: string;
   summary: string;
   tags: string[];
-  category: string; // Add this line to include the category property
+  category: string;
 }
 
 interface BlogProps {
@@ -22,8 +21,7 @@ interface BlogProps {
 export default function Blog({ allPostsData }: BlogProps) {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [decryptName, setDecryptName] = useState<string>();
-  //Tabs
-  const [activeTab, setActiveTab] = useState("daily"); // New state for active tab
+  const [activeTab, setActiveTab] = useState("daily");
   const categories = ["Daily", "Studying", "Travel"];
 
   const handleTagClick = (tag: string) => {
@@ -36,17 +34,15 @@ export default function Blog({ allPostsData }: BlogProps) {
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab.toLowerCase());
-    setSelectedTags([]); // Reset tags when changing category
+    setSelectedTags([]);
   };
 
-  // Filter posts by category
   const filteredPostsByCategory = useMemo(() => {
     return allPostsData.filter(
       (post) => post.category.toLowerCase() === activeTab
     );
   }, [allPostsData, activeTab]);
 
-  // Get unique secondary tags for the selected category
   const secondaryTags = useMemo(() => {
     const tagsSet = new Set<string>();
     filteredPostsByCategory.forEach((post) => {
@@ -59,7 +55,6 @@ export default function Blog({ allPostsData }: BlogProps) {
     return Array.from(tagsSet);
   }, [filteredPostsByCategory, activeTab]);
 
-  // Further filter posts by selected tags
   const finalFilteredPosts = useMemo(() => {
     return selectedTags.length === 0
       ? filteredPostsByCategory
@@ -67,66 +62,6 @@ export default function Blog({ allPostsData }: BlogProps) {
           selectedTags.every((tag) => post.tags.includes(tag))
         );
   }, [filteredPostsByCategory, selectedTags]);
-
-  useEffect(() => {
-    async function decryptAndRetrieveData() {
-      try {
-        console.log("Attempting to retrieve data from localStorage");
-        console.log("localStorage keys available:", Object.keys(localStorage));
-
-        const encryptedData = localStorage.getItem("encryptedUserInfo");
-        const iv = localStorage.getItem("userInfoIV");
-        const base64Key = localStorage.getItem("encryptionKey");
-
-        if (!encryptedData || !iv || !base64Key) {
-          console.error("No encrypted data, IV, or key found in Local Storage");
-          console.log("encryptedData:", encryptedData);
-          console.log("IV:", iv);
-          console.log("Key:", base64Key);
-          return;
-        }
-
-        const encryptedBuffer = Uint8Array.from(atob(encryptedData), (c) =>
-          c.charCodeAt(0)
-        );
-        const ivBuffer = Uint8Array.from(atob(iv), (c) => c.charCodeAt(0));
-        const keyBuffer = Uint8Array.from(atob(base64Key), (c) =>
-          c.charCodeAt(0)
-        );
-
-        // Import the key
-        const key = await window.crypto.subtle.importKey(
-          "raw",
-          keyBuffer,
-          {
-            name: "AES-GCM",
-            length: 256,
-          },
-          true,
-          ["decrypt"]
-        );
-
-        const decryptedData = await window.crypto.subtle.decrypt(
-          {
-            name: "AES-GCM",
-            iv: ivBuffer,
-          },
-          key,
-          encryptedBuffer
-        );
-
-        const dec = new TextDecoder();
-        const decryptedStr = dec.decode(decryptedData);
-        const userInfo = JSON.parse(decryptedStr);
-        console.log(userInfo);
-        setDecryptName(userInfo.name);
-      } catch (error) {
-        console.error("Error decrypting or retrieving data:", error);
-      }
-    }
-
-    decryptAndRetrieveData();
-  }, []);
 
   return (
     <Layout>
@@ -136,7 +71,6 @@ export default function Blog({ allPostsData }: BlogProps) {
       <h1 className="mb-4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
         Blog
       </h1>
-      <p>{decryptName}</p>
       <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
         Welcome to my personal blog, where I share my thoughts and experiences
         on various topics including technology, travel, personal development and
