a699e72b0d95d011800cad07ff32801dc597bcbd
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2025-03-25 14:11:45 +0900

    Update resume.ts

--- src/app/constants/resume.ts
+++ src/app/constants/resume.ts
@@ -1,7 +1,6 @@
 export const experiences = [
-  { start: "2024-01-01", end: null }, // Ongoing job
-  { start: "2023-07-01", end: "2023-10-01" },
-  { start: "2022-07-01", end: "2023-06-01" },
-  { start: "2021-01-01", end: "2022-05-01" },
-  // Add other experiences as needed
+  { start: "2024-01-08", end: null }, // e8ight
+  { start: "2023-06-16", end: "2023-10-13" }, // flashee
+  { start: "2022-07-12", end: "2023-06-15" }, // iclinic
+  { start: "2021-01-07", end: "2022-05-15" }, // catalyx
 ];
