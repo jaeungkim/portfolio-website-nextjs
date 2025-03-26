1a8c158fbc12f9b5d1633210b1c71d708b97fe03
jaeungkim <jaewoongkim95@gmail.com>
2025-03-26 11:05:18 +0900

    feature: moved image fetching from local to gcloud cdn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/jaeungkim/**",
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
