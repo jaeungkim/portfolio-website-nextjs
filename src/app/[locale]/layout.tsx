0f127ad063c2187a412b9e22714718eb8f8ef040
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2025-04-09 07:39:49 +0900

    layout.tsx 업데이트

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,7 +28,7 @@ export default async function PortfolioLayout({
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <div className="size-full bg-neutral-100 dark:bg-neutral-900">
+      <div className="bg-neutral-100 dark:bg-neutral-850">
 <Navbar />
       <main className="w-full grow relative mx-auto max-w-5xl px-8">
         <Suspense>{children}</Suspense>
