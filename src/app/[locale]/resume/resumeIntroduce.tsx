f77398df59ab9111aade4d0e09fde202244e00e6
jaeungkim <jaewoongkim95@gmail.com>
2025-04-19 12:43:14 +0900

    chore: update on en.json translation

--- src/app/[locale]/resume/resumeIntroduce.tsx
+++ src/app/[locale]/resume/resumeIntroduce.tsx
@@ -13,7 +13,7 @@ export default function resumeIntroduce() {
       {/* Introduce */}
       <div className="md:grid md:grid-cols-4 md:gap-4">
         <ResumeTitle title="Introduce" />
-        <div className="col-span-3 flex flex-col gap-2 dark:text-neutral-300 text-neutral-700">
+        <div className="col-span-3 flex flex-col gap-2 dark:text-neutral-300 text-neutral-700 mt-6 md:mt-0">
           <p className="">{t("paragraph1")}</p>
           <p className="">{t("paragraph2")}</p>
           <p className="">{t("paragraph3")}</p>
