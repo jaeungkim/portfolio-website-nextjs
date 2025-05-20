8516947a42ba78d35598d0f5c60b7aa14837c063
jaeungkim <jaewoongkim95@gmail.com>
2025-05-20 21:01:22 +0900

    chore: archived some posts

--- src/app/gantt-chart/page.tsx
+++ src/app/gantt-chart/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
-import { useTheme } from "next-themes";
 import { ReactGanttChart } from "@jaeungkim/gantt-chart";
 
 export default function Page() {
-  const { setTheme } = useTheme();
-
-  useEffect(() => {
-    setTheme("light");
-  }, [setTheme]);
-
   return (
     <div className="force-light relative overflow-auto w-full h-svh">
       <ReactGanttChart tasks={[]} columWidth="w-fit" ganttHeight="100%" />
