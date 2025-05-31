7e3d149a09b8f67b881db8cc605bfd2708dad647
jaeungkim <jaewoongkim95@gmail.com>
2025-05-31 13:39:39 +0900

    chore: fixing deployment error

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const { locale } = await params;
+  const { locale } = params;
 
   if (!["en", "kr"].includes(locale)) {
     notFound();
