ba4c9d6e0fa8fc57ffb08cf65660ebe831c59c7d
jaeungkim <jaewoongkim95@gmail.com>
2025-06-01 11:24:39 +0900

    chore: removed archived mdx and added 2 travel posts

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function BlogPage() {
   return (
-    <div className="px-4 py-10 max-w-5xl mx-auto">
-      <h1 className="mb-8 text-4xl font-bold text-neutral-800 dark:text-neutral-100 sm:text-5xl">
-        Blog
+    <div className="py-4 max-w-5xl mx-auto">
+      <h1 className="mb-12 text-4xl font-bold text-neutral-800 dark:text-neutral-100 sm:text-5xl">
+        Travel
       </h1>
 
       <Suspense fallback={<p className="text-center mt-10">Loading posts...</p>}>
