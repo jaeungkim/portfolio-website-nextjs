200d903f7b378d2799950963f5cd4b806cc73638
jaeungkim <jaewoongkim95@gmail.com>
2025-09-14 19:32:49 +0900

    chore: working on design and ui/ux now

--- src/components/mobile-wedding/constants.ts
+++ src/components/mobile-wedding/constants.ts
@@ -5,7 +5,7 @@ export const ANIMATION_CONFIG = {
   duration: 1.2,
   ease: "power2.out",
   scrollTrigger: {
-    start: "top 90%",
+    start: "top 80%",
     end: "bottom 40%",
     toggleActions: "play none none reverse",
     scrub: false,
